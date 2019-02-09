06f9dadad1319cd166c4d6912f6f55a09a9fcda5
SN <gohsuning@gmail.com>
2019-02-09 16:13:29 -0500

    Initial commit, creating project environment (using React framework)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './static/logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -20,6 +23,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+
       </div>
     );
   }
