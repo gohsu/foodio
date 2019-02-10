bc1db30b5e7b9cce193e60a92ad2ee73054a5683
SN <gohsuning@gmail.com>
2019-02-09 19:30:01 -0500

    Added function pulling Yelp ratings

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} alt="logo" />
         </header>
-
       </div>
     );
   }
---
+++ src/Rating/index.js
@@ -0,0 +1,30 @@
+/*
+This class pulls restaurant ratings from Yelp and Google,
+returns a weighted average of both of these.
+
+Yelp search is done by business ID
+ */
+
+let yelpAPI = require('yelp-api');
+
+// Create a new yelpAPI object with your API key
+let apiKey = 'JsK559hp0ZfSJrH0DXGJjsbMolHQ-CM0E60HMSV5SlmJ_sKgdXutZoL8aS0TA25Se-C6yNM1pvhLSL-5L5ro6B7QJEnlpF0ifrtrTL_dLMhzS4jM8HS6CrnFFzdfXHYx';
+let yelp = new yelpAPI(apiKey);
+
+// Set any parameters, if applicable (see API documentation for allowed params)
+let businessId = 'drNjH8Z63cgp8FWXh2hahA';
+
+// Call the endpoint
+yelp.query(`businesses/${businessId}`)
+    .then(response => {
+        // Success
+        var fields = JSON.parse(response);
+        const yelpRating = fields.rating;
+        const yelpReviewCount = fields.review_count;
+        console.log(yelpRating+"\n");
+        console.log(yelpReviewCount);
+    })
+    .catch(err => {
+        // Failure
+        console.log(err);
+    });
